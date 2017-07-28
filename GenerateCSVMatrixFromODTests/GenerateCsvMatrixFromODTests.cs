b2101b05641c9e440ff6a46fc4342448574724f0
patr5651 <pstevens@esri.com>
2017-07-28 11:04:40 -0700

    First tests

---
+++ GenerateCSVMatrixFromODTests/GenerateCsvMatrixFromODTests.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GenerateCSVMatrixFromOD;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenerateCSVMatrixFromOD.Tests
+{
+  [TestClass]
+  public sealed class GenerateCsvMatrixFromODTests
+  {
+    [TestMethod]
+    public void InvalidCommandLineNullArgsTest()
+    {
+      try
+      {
+        GenerateCsvMatrixFromOD.Main(null);
+        Assert.Fail("Expected exception to be thrown");
+      }
+      catch (Exception e)
+      {
+        Assert.AreEqual("Command line format: <layer file name>", e.Message);
+      }
+    }
+
+    [TestMethod]
+    public void InvalidCommandLineNoArgsTest()
+    {
+      try
+      {
+        GenerateCsvMatrixFromOD.Main(new string[] {});
+        Assert.Fail("Expected exception to be thrown");
+      }
+      catch (Exception e)
+      {
+        Assert.AreEqual("Command line format: <layer file name>", e.Message);
+      }
+    }
+
+    [TestMethod]
+    public void InvalidCommandLineTooManyArgsTest()
+    {
+      try
+      {
+        GenerateCsvMatrixFromOD.Main(new [] {"arg1", "arg2"});
+        Assert.Fail("Expected exception to be thrown");
+      }
+      catch (Exception e)
+      {
+        Assert.AreEqual("Command line format: <layer file name>", e.Message);
+      }
+    }
+
+    [TestMethod]
+    public void InvalidCommandLineNoLayerTest()
+    {
+      const string layerFile = @"c:\temp\Nope.lyr";
+      try
+      {
+        GenerateCsvMatrixFromOD.Main(new[] { layerFile });
+        Assert.Fail("Expected exception to be thrown");
+      }
+      catch (Exception e)
+      {
+        Assert.AreEqual($"Layer file does not exist: {layerFile}", e.Message);
+      }
+    }
+  }
+}
